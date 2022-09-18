0a03ec0a4e1ddefabd5201d059332855705e4771
JuanPCT <89850618+JuanPCT@users.noreply.github.com>
2022-09-18 15:43:47 -0500

    editar y borrar registros de la BD

--- router.js
+++ router.js
@@ -17,7 +17,30 @@ router.get('/create', (req, res)=>{
     res.render('create');
 });
 
+router.get('/edit/:id', (req,res)=>{
+    const id = req.params.id;
+    conexion.query('select * from user where id=?',[id], (error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('edit.ejs', {user:results[0]});
+        }
+    });    
+});
+
+router.get('/delete/:id', (req,res)=>{
+    const id = req.params.id;
+    conexion.query('delete from user where id=?', [id], (error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
 const crud = require('./controles/crud');
 router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
